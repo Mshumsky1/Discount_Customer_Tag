059a7f214cbe2cc5a53b11cac294c45e93ca962b
Mshumsky1 <112615574+Mshumsky1@users.noreply.github.com>
2023-06-20 14:45:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,63 @@
-export default /**
-* @param {InputQuery} input
-* @returns {FunctionResult}
+import { DiscountApplicationStrategy } from "../generated/api";
+// Use JSDoc annotations for type safety
+/**
+* @typedef {import("../generated/api").InputQuery} InputQuery
+* @typedef {import("../generated/api").FunctionResult} FunctionResult
+* @typedef {import("../generated/api").Target} Target
+* @typedef {import("../generated/api").ProductVariant} ProductVariant
 */
-  (input) => {
-    const { cart } = input;
-    const { customer } = cart.buyerIdentity;
+/**
+* @type {FunctionResult}
+*/
+const EMPTY_DISCOUNT = {
+  discountApplicationStrategy: DiscountApplicationStrategy.First,
+  discounts: [],
+};
 
-    if (!customer || !customer.hasAnyTag('VIP')) {
-      // Return an empty discount if the customer is not a VIP
-      console.error("Unauthorized access: customer is not a VIP.");
-      return EMPTY_DISCOUNT;
-    }
+export default (input) => {
+  console.log("Input data:", JSON.stringify(input));
 
-    const targets = cart.lines
-      // Only include cart lines with a quantity of two or more
-      // and a targetable product variant
-      .filter(line => line.quantity >= 4 &&
-        line.merchandise.__typename == "ProductVariant")
-      .map(line => {
-        const variant = /** @type {ProductVariant} */ (line.merchandise);
-        return /** @type {Target} */ ({
-          // Use the variant ID to create a discount target
-          productVariant: {
-            id: variant.id
-          }
-        });
-      });
+  const { cart } = input;
+  if (!cart.buyerIdentity) {
+    console.error("Unauthorized access: cart.buyerIdentity is missing.");
+    return EMPTY_DISCOUNT;
+  }
+  const { customer } = cart.buyerIdentity;
 
-    if (!targets.length) {
-      // You can use STDERR for debug logs in your function
-      console.error("No cart lines qualify for volume discount.");
-      return EMPTY_DISCOUNT;
-    }
+  
+  if (!customer || customer.hasAnyTag !== true){
+    // Return an empty discount if the customer is not a VIP
+    console.error("Unauthorized access: customer is not a VIP.");
+    return EMPTY_DISCOUNT;
+  }
 
-    // The @shopify/shopify_function package applies JSON.stringify() to your function result
-    // and writes it to STDOUT
-    return {
-      discounts: [
-        {
-          // Apply the discount to the collected targets
-          targets,
-          // Define a percentage-based discount
-          value: {
-            percentage: {
-              value: "50.0"
-            }
-          }
-        }
-      ],
-      discountApplicationStrategy: DiscountApplicationStrategy.First
-    };
+  const targets = cart.lines
+    .filter(line => line.quantity >= 1 && line.merchandise.__typename == "ProductVariant")
+    .map(line => {
+      const variant = line.merchandise;
+      return {
+        productVariant: {
+          id: variant.id,
+        },
+      };
+    });
+
+  if (!targets.length) {
+    console.error("No cart lines qualify for volume discount.");
+    return EMPTY_DISCOUNT;
+  }
+
+  return {
+    discounts: [
+      {
+        targets,
+        value: {
+          percentage: {
+            value: "50.0",
+          },
+        },
+      },
+    ],
+    discountApplicationStrategy: DiscountApplicationStrategy.First,
   };
+};
